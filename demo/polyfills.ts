6a1defcd6e584edafef442d3e75aa76f3fc61a5a
omostepaniuk <o.mostepaniuk@gmail.com>
2018-09-06 17:23:32 +0300

    Prototype of the polyfill-based drag-and-drop

--- demo/polyfills.ts
+++ demo/polyfills.ts
@@ -9,3 +9,13 @@
 import 'core-js';
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
+
+// HTML5 Drag & Drop
+import {polyfill} from 'mobile-drag-drop';
+import {scrollBehaviourDragImageTranslateOverride} from 'mobile-drag-drop/scroll-behaviour';
+polyfill({
+    dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride,
+    holdToDrag: 200
+});
+
+window.addEventListener( 'touchmove', function() {});
